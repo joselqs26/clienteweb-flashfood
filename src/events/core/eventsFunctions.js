b2d3e84a4d716f118a0067479606a4eff59cbdc3
Jose Luis Quintero <joseluisquintero06@gmail.com>
2023-04-22 10:49:50 -0500

    Se implementa el envío de información hacia el Event Hub, en espera de encriptarla

--- src/events/core/eventsFunctions.js
+++ src/events/core/eventsFunctions.js
@@ -1,18 +1,16 @@
 const { EventHubProducerClient } = require("@azure/event-hubs");
-const { DefaultAzureCredential } = require("@azure/identity");
 
 // Event hubs 
 const fullyQualifiedNamespace = "Endpoint=sb://eventhub-flashfood.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessPolicy;SharedAccessKey=D7s1PbsGugJeWQaQpiwGJw199QvKqfIcX+AEhFJNPI4=;EntityPath=eventhub-flashfood"; 
 const eventHubName = "eventhub-flashfood";
 
 async function sendEvent(eventData) {
-
   // Create a producer client to send messages to the event hub.
   const producer = new EventHubProducerClient(fullyQualifiedNamespace, eventHubName);
 
   // Prepare a batch of three events.
   const batch = await producer.createBatch();
-  batch.tryAdd( eventData );
+  batch.tryAdd( { body: eventData } );
 
   // Send the batch to the event hub.
   await producer.sendBatch(batch);
--- src/events/enviarLogin.js
+++ src/events/enviarLogin.js
@@ -1,20 +1,8 @@
-import { sendEvent } from "./core/eventsFunctions";
+import { sendEvent } from "./core/eventsFunctions.js";
 
 function enviarLogin(email, password) {
     const infoLogin = {email, password};
-    const encodedLogin = btoa(infoLogin);
-    
-    let result;
-
-    sendEvent( encodedLogin )
-      .then((response) => {
-        result = response
-      })
-      .catch((err) => {
-        result = {error: 'Error login'};
-      });
-
-    return result;
+    sendEvent( infoLogin );
 }
 
 export {enviarLogin};
